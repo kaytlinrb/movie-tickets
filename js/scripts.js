dd4633d08ad0b313e28e54153d7bf2570d177997
Katie Borel and Starlynne Start <katie@>
2017-04-04 12:03:25 -0700

    Add functionality for returning movie selection and price to user, add logic to clear list on resubmit, add styling for background and header.

--- js/scripts.js
+++ js/scripts.js
@@ -8,41 +8,32 @@ function Ticket(movie, age, time){
 Ticket.prototype.priceAssign = function() {
 
   var price = 0;
-  var price2 = 0;
-  var price3 = 0;
-    for (var i =0; i<1 ; i++){
-      if (this.movie === "Lego Batman") {
-        price = price + 5;
-      } else if (this.movie === "Batman Begins") {
-        price = price + 3;
-      } else {
-        price = price + 1;
-      }
-      console.log(price);
-    }
 
-    for (var i =0; i<1 ; i++){
-      if (this.age === "17 or Younger") {
-        price2 = price2 + 2;
-      } else if (this.movie === "18-54") {
-        price2 = price2 + 3;
-      } else {
-        price2 = price2 + 1;
-      }
-    }
-    console.log(price2);
+  if (this.movie === "Lego Batman") {
+    price = price + 5;
+  } else if (this.movie === "Batman Begins") {
+    price = price + 3;
+  } else {
+    price = price + 1;
+  }
 
-      for (var i =0; i<1 ; i++) {
-        if (this.time === "11AM") {
-        price3 = price3 + 1;
-        } else if (this.movie === "6PM") {
-        price3 = price3 + 3;
-        } else {
-        price3 = price3 + 2;
-        }
-    }
-    console.log(price3);
-    return price + price2 + price3
+  if (this.age === "17 or Younger") {
+    price = price + 2;
+  } else if (this.movie === "18-54") {
+    price = price + 3;
+  } else {
+    price = price + 1;
+  }
+
+  if (this.time === "11AM") {
+  price = price + 1;
+  } else if (this.movie === "6PM") {
+  price = price + 3;
+  } else {
+  price = price + 2;
+  }
+
+  return price
 }
 
 
@@ -53,14 +44,16 @@ Ticket.prototype.priceAssign = function() {
 $(function() {
   $("#ticket-form").submit(function(event){
     event.preventDefault();
-
+    $("li").remove();
     var movieChoice = $("#movie-choice").val();
     var ageChoice = $("#age").val();
     var timeChoice = $("#time").val();
 
     var newTicket = new Ticket(movieChoice, ageChoice, timeChoice);
     // var ticketPrice = priceAssign(newTicket);
-    console.log(newTicket.priceAssign());
+    var ticketPrice = newTicket.priceAssign();
+    $("#ticket-info").append("<li>" + movieChoice + "</li>" + "<li>" + timeChoice + "</li>"+"<li>" + "$ " + ticketPrice + "</li>");
+    $("#show-ticket").show();
   });
 
 });
